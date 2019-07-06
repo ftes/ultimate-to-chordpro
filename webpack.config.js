dc2fd8d6dee37a192e68cfd73b7037c64746916d
Fredrik Teschke <f@ftes.de>
2019-07-06 19:33:02 +0200

    :sparkles: Optimize for search engines (description, favicon)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebappWebpackPlugin = require("webapp-webpack-plugin");
+
 module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new WebappWebpackPlugin('./src/image/guitar.png')
+  ],
   devServer: {
     contentBase: "./dist"
   },
