aec9b2db82bc3e23b08ea03c106bbe4bfd8b727e
Fredrik Teschke <f@ftes.de>
2019-07-06 08:33:52 +0200

    :sparkles: Add fork ribbon and auto-convert

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import ChordSheetJS from '@ftes/chordsheetjs';
+import ChordSheetJS from "@ftes/chordsheetjs";
+import "./style/index.css"
 
 const parser = new ChordSheetJS.UltimateGuitarParser({
   preserveWhitespace: false
-})
-const formatter = new ChordSheetJS.ChordProFormatter()
+});
+const formatter = new ChordSheetJS.ChordProFormatter();
 
 function convert() {
-  const input = document.getElementById('ultimate').value
-  const parsed = parser.parse(input)
+  const input = document.getElementById("ultimate").value;
+  const parsed = parser.parse(input);
   const output = formatter.format(parsed);
-  document.getElementById('chordpro').value = output
+  document.getElementById("chordpro").value = output;
 }
 
-document.getElementById('convert').addEventListener('click', convert)
-convert()
+let toggleState = document.getElementById("toggle").checked;
+function setToggle(e) {
+  toggleState = e.target.checked;
+}
+function convertIfToggled() {
+  if (toggleState) {
+    convert();
+  }
+}
+
+document.getElementById("convert").addEventListener("click", convert);
+document.getElementById("toggle").addEventListener("change", setToggle);
+document.getElementById("ultimate").addEventListener("keyup", convertIfToggled);
+convert();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   devServer: {
     contentBase: "./dist"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
