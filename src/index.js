bb56359342c482c3883f9b7028adbb4852d181e0
Fredrik Teschke <f@ftes.de>
2023-11-27 10:39:02 +0100

    npx standard --fix

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
-import ChordSheetJS from "chordsheetjs";
-import LatexFormatter from "./latex-formatter";
-import "./style/index.css";
+import ChordSheetJS from 'chordsheetjs'
+import LatexFormatter from './latex-formatter'
+import './style/index.css'
 
 const parsers = {
   chordpro: new ChordSheetJS.ChordProParser(),
   ultimate: new ChordSheetJS.UltimateGuitarParser({
-    preserveWhitespace: false,
-  }),
-};
+    preserveWhitespace: false
+  })
+}
 const formatters = {
   chordpro: new ChordSheetJS.ChordProFormatter(),
   latex: new LatexFormatter(),
-  ultimate: new ChordSheetJS.TextFormatter(),
-};
-
-function convert() {
-  const input = document.getElementById("ultimate").value;
-
-  const fromFormatEl = document.getElementById("from-format");
-  const toFormatEl = document.getElementById("to-format");
-  const fromFormat = fromFormatEl.options[fromFormatEl.selectedIndex].value;
-  const toFormat = toFormatEl.options[toFormatEl.selectedIndex].value;
-
-  const parsed = parsers[fromFormat].parse(input);
-  const output = formatters[toFormat].format(parsed);
-  document.getElementById("chordpro").value = output;
+  ultimate: new ChordSheetJS.TextFormatter()
 }
 
-let toggleState = document.getElementById("toggle").checked;
-function setToggle(e) {
-  toggleState = e.target.checked;
+function convert () {
+  const input = document.getElementById('ultimate').value
+
+  const fromFormatEl = document.getElementById('from-format')
+  const toFormatEl = document.getElementById('to-format')
+  const fromFormat = fromFormatEl.options[fromFormatEl.selectedIndex].value
+  const toFormat = toFormatEl.options[toFormatEl.selectedIndex].value
+
+  const parsed = parsers[fromFormat].parse(input)
+  const output = formatters[toFormat].format(parsed)
+  document.getElementById('chordpro').value = output
 }
-function convertIfToggled() {
+
+let toggleState = document.getElementById('toggle').checked
+function setToggle (e) {
+  toggleState = e.target.checked
+}
+function convertIfToggled () {
   if (toggleState) {
-    convert();
+    convert()
   }
 }
 
-document.getElementById("convert").addEventListener("click", convert);
-document.getElementById("toggle").addEventListener("change", setToggle);
-document.getElementById("ultimate").addEventListener("keyup", convertIfToggled);
-document.getElementById("from-format").addEventListener("change", convert);
-document.getElementById("to-format").addEventListener("change", convert);
-convert();
+document.getElementById('convert').addEventListener('click', convert)
+document.getElementById('toggle').addEventListener('change', setToggle)
+document.getElementById('ultimate').addEventListener('keyup', convertIfToggled)
+document.getElementById('from-format').addEventListener('change', convert)
+document.getElementById('to-format').addEventListener('change', convert)
+convert()
