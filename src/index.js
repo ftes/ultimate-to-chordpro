2ac10f8523d82b9bedbce2dec81d96ac271f820d
iiTzArcur <zefanjaj@gmail.com>
2024-07-20 12:18:31 +0200

    add fullscreen buttons to see result with correct font in fullscreen for phones

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import ChordSheetJS from 'chordsheetjs'
-import LatexFormatter from './latex-formatter'
-import PlanningcenterFormatter from './planningcenter-formatter'
-import LyricsFormatter from "./lyrics-formatter"
-import './style/index.css'
+import { Modal } from 'bootstrap';
+import ChordSheetJS from 'chordsheetjs';
+import LatexFormatter from './latex-formatter';
+import LyricsFormatter from "./lyrics-formatter";
+import PlanningcenterFormatter from './planningcenter-formatter';
+import './style/index.css';
 
 function convert() {
   const parsers = {
@@ -55,4 +56,19 @@ document.getElementById('from-format').addEventListener('change', convert)
 document.getElementById('whitespace-toggle').addEventListener('change', convert)
 document.getElementById('to-format').addEventListener('change', convert)
 document.getElementById("transpose-amount").addEventListener('change', convert)
+const modal = new Modal(document.getElementById('modal'));
+document.getElementById("fullscreen-input").addEventListener("click", () => {
+  document.getElementById("modal-title").innerText = "Input"
+  document.getElementById("modal-body").innerHTML = document.getElementById('ultimate').value.replace(/(?:\r\n|\r|\n)/g, '<br>').replace(/\s/g, '&nbsp;');
+  modal.toggle();
+})
+document.getElementById("fullscreen-output").addEventListener("click", () => {
+  document.getElementById("modal-title").innerText = "Output"
+  document.getElementById("modal-body").innerHTML = document.getElementById('chordpro').value.replace(/(?:\r\n|\r|\n)/g, '<br>').replace(/\s/g, '&nbsp;');
+  modal.toggle();
+})
+const closeButtons = document.getElementsByClassName("close-modal");
+for (let i = 0; i < closeButtons.length; i++) {
+  closeButtons.item(i).addEventListener("click", () => modal.toggle())
+}
 convert()
