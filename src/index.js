3dd59033a392880624455e055b65533bbb02950c
Fredrik Teschke <f@ftes.de>
2026-02-13 15:33:26 +0100

    Track input/output formats

--- src/index.js
+++ src/index.js
@@ -1,45 +1,79 @@
-import ChordSheetJS from 'chordsheetjs'
-import LatexFormatter from './latex-formatter'
-import './style/index.css'
+/* global gtag */
+import ChordSheetJS from "chordsheetjs";
+import LatexFormatter from "./latex-formatter";
+import "./style/index.css";
 
 const parsers = {
   chordpro: new ChordSheetJS.ChordProParser(),
   ultimate: new ChordSheetJS.UltimateGuitarParser({
-    preserveWhitespace: false
-  })
-}
+    preserveWhitespace: false,
+  }),
+};
 const formatters = {
   chordpro: new ChordSheetJS.ChordProFormatter(),
   latex: new LatexFormatter(),
-  ultimate: new ChordSheetJS.TextFormatter()
+  ultimate: new ChordSheetJS.TextFormatter(),
+};
+
+let formatTracked = false;
+
+function trackFormatConversion() {
+  if (formatTracked) return;
+  const input = document.getElementById("ultimate").value;
+  if (!input.trim()) return;
+  formatTracked = true;
+  const fromFormat = document.getElementById("from-format").value;
+  const toFormat = document.getElementById("to-format").value;
+  gtag("event", "format_conversion", {
+    from_format: fromFormat,
+    to_format: toFormat,
+  });
 }
 
-function convert () {
-  const input = document.getElementById('ultimate').value
-
-  const fromFormatEl = document.getElementById('from-format')
-  const toFormatEl = document.getElementById('to-format')
-  const fromFormat = fromFormatEl.options[fromFormatEl.selectedIndex].value
-  const toFormat = toFormatEl.options[toFormatEl.selectedIndex].value
-
-  const parsed = parsers[fromFormat].parse(input)
-  const output = formatters[toFormat].format(parsed)
-  document.getElementById('chordpro').value = output
+function resetFormatTracking() {
+  formatTracked = false;
 }
 
-let toggleState = document.getElementById('toggle').checked
-function setToggle (e) {
-  toggleState = e.target.checked
+function convert() {
+  const input = document.getElementById("ultimate").value;
+
+  const fromFormatEl = document.getElementById("from-format");
+  const toFormatEl = document.getElementById("to-format");
+  const fromFormat = fromFormatEl.options[fromFormatEl.selectedIndex].value;
+  const toFormat = toFormatEl.options[toFormatEl.selectedIndex].value;
+
+  const parsed = parsers[fromFormat].parse(input);
+  const output = formatters[toFormat].format(parsed);
+  document.getElementById("chordpro").value = output;
 }
-function convertIfToggled () {
+
+function convertAndTrack() {
+  convert();
+  trackFormatConversion();
+}
+
+function convertAndTrackIfToggled() {
   if (toggleState) {
-    convert()
+    convertAndTrack();
   }
 }
 
-document.getElementById('convert').addEventListener('click', convert)
-document.getElementById('toggle').addEventListener('change', setToggle)
-document.getElementById('ultimate').addEventListener('keyup', convertIfToggled)
-document.getElementById('from-format').addEventListener('change', convert)
-document.getElementById('to-format').addEventListener('change', convert)
-convert()
+let toggleState = document.getElementById("toggle").checked;
+function setToggle(e) {
+  toggleState = e.target.checked;
+}
+
+document.getElementById("convert").addEventListener("click", convertAndTrack);
+document.getElementById("toggle").addEventListener("change", setToggle);
+document
+  .getElementById("ultimate")
+  .addEventListener("keyup", convertAndTrackIfToggled);
+document.getElementById("from-format").addEventListener("change", function () {
+  resetFormatTracking();
+  convertAndTrack();
+});
+document.getElementById("to-format").addEventListener("change", function () {
+  resetFormatTracking();
+  convertAndTrack();
+});
+convert();
--- src/latex-formatter.js
+++ src/latex-formatter.js
@@ -7,22 +7,22 @@ import Tag, {
   START_OF_CHORUS,
   END_OF_CHORUS,
   START_OF_VERSE,
-  END_OF_VERSE
-} from 'chordsheetjs/lib/chord_sheet/tag'
-import ChordLyricsPair from 'chordsheetjs/lib/chord_sheet/chord_lyrics_pair'
+  END_OF_VERSE,
+} from "chordsheetjs/lib/chord_sheet/tag";
+import ChordLyricsPair from "chordsheetjs/lib/chord_sheet/chord_lyrics_pair";
 
-const NEW_LINE = '\n'
-const flatMap = (arr, fn) => arr.reduce((acc, x) => [...acc, ...fn(x)], [])
+const NEW_LINE = "\n";
+const flatMap = (arr, fn) => arr.reduce((acc, x) => [...acc, ...fn(x)], []);
 
 /**
  * Formats a song into a .tex file for the Latex songs package.
  * http://songs.sourceforge.net/docs.html
  */
 class LatexFormatter {
-  isHeaderTag (item) {
+  isHeaderTag(item) {
     return (
       item instanceof Tag && [TITLE, SUBTITLE, ARTIST].indexOf(item.name) !== -1
-    )
+    );
   }
 
   /**
@@ -30,90 +30,90 @@ class LatexFormatter {
    * @param {Song} song The song to be formatted
    * @returns {string} The .tex file contents as string
    */
-  format (song) {
+  format(song) {
     return [
       this.formatHeaderTags(song),
       this.formatOther(song),
-      '\\endsong'
-    ].join(NEW_LINE)
+      "\\endsong",
+    ].join(NEW_LINE);
   }
 
-  formatHeaderTags (song) {
+  formatHeaderTags(song) {
     const headerTags = flatMap(song.lines, (line) => line.items)
       .filter(this.isHeaderTag)
       .reduce(
         (tmp, tag) => ({
           ...tmp,
-          [tag.name]: tag
+          [tag.name]: tag,
         }),
         {}
-      )
-    const title = headerTags.title && headerTags.title.value
-    const subtitle = headerTags.subtitle && headerTags.subtitle.value
-    const artist = headerTags.artist ? headerTags.artist.value : ''
-    const titleString = title + (subtitle ? ` \\\\ ${subtitle}` : '')
-    return `\\beginsong{${titleString}}[by={${artist}}]`
+      );
+    const title = headerTags.title && headerTags.title.value;
+    const subtitle = headerTags.subtitle && headerTags.subtitle.value;
+    const artist = headerTags.artist ? headerTags.artist.value : "";
+    const titleString = title + (subtitle ? ` \\\\ ${subtitle}` : "");
+    return `\\beginsong{${titleString}}[by={${artist}}]`;
   }
 
-  formatOther (song) {
-    return song.lines.map((line) => this.formatLine(line)).join(NEW_LINE)
+  formatOther(song) {
+    return song.lines.map((line) => this.formatLine(line)).join(NEW_LINE);
   }
 
-  formatLine (line) {
-    return line.items.map((item) => this.formatItem(item)).join('')
+  formatLine(line) {
+    return line.items.map((item) => this.formatItem(item)).join("");
   }
 
-  formatItem (item) {
+  formatItem(item) {
     if (this.isHeaderTag(item)) {
-      return ''
+      return "";
     } else if (item instanceof Tag) {
-      return this.formatTag(item)
+      return this.formatTag(item);
     } else if (item instanceof ChordLyricsPair) {
-      return this.formatChordLyricsPair(item)
+      return this.formatChordLyricsPair(item);
     }
 
-    return ''
+    return "";
   }
 
-  formatTag (tag) {
+  formatTag(tag) {
     switch (tag.name) {
       case COMMENT:
-        return `\\textcomment{${tag.value}}`
+        return `\\textcomment{${tag.value}}`;
       case CAPO:
-        return `\\capo{${tag.value}}`
+        return `\\capo{${tag.value}}`;
       case START_OF_CHORUS:
-        return '\\beginchorus'
+        return "\\beginchorus";
       case END_OF_CHORUS:
-        return '\\endchorus'
+        return "\\endchorus";
       case START_OF_VERSE:
-        return '\\beginverse'
+        return "\\beginverse";
       case END_OF_VERSE:
-        return '\\endverse'
+        return "\\endverse";
       default:
         return tag.hasValue()
           ? `\\textcomment{${tag.originalName}: ${tag.value}}`
-          : `\\textcomment{${tag.originalName}}`
+          : `\\textcomment{${tag.originalName}}`;
     }
   }
 
-  formatChordLyricsPair (chordLyricsPair) {
+  formatChordLyricsPair(chordLyricsPair) {
     return [
       this.formatChordLyricsPairChords(chordLyricsPair),
-      this.formatChordLyricsPairLyrics(chordLyricsPair)
-    ].join('')
+      this.formatChordLyricsPairLyrics(chordLyricsPair),
+    ].join("");
   }
 
-  formatChordLyricsPairChords (chordLyricsPair) {
+  formatChordLyricsPairChords(chordLyricsPair) {
     if (chordLyricsPair.chords) {
-      return `\\[${chordLyricsPair.chords}]`
+      return `\\[${chordLyricsPair.chords}]`;
     }
 
-    return ''
+    return "";
   }
 
-  formatChordLyricsPairLyrics (chordLyricsPair) {
-    return chordLyricsPair.lyrics || ''
+  formatChordLyricsPairLyrics(chordLyricsPair) {
+    return chordLyricsPair.lyrics || "";
   }
 }
 
-export default LatexFormatter
+export default LatexFormatter;
